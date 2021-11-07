3c9d1e1b5d2b77ac55f305fc258101a78103a33a
Ritesh <arora.ritesh.95@gmail.com>
2021-11-07 10:50:15 +0530

    models

---
+++ model/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const productSchema = new mongoose.Schema({
+  name: String,
+  quantity: Number,
+  price: Number,
+  image: String,
+  id: Object,
+});
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: String,
+  lastName: String,
+  isAdmin: Boolean,
+  admin: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" },
+  products: [productSchema],
+});
+
+userSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model("User", userSchema);
